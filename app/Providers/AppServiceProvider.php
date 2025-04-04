ddb83752e8c762d7eb8f392f3672e5016fea8a33
DereckGeorge <eridericgeorge@gmail.com>
2025-04-04 20:38:26 +0300

    declared recent work variable in appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Work;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            $recentWorks = Work::latest()
+                ->take(6)
+                ->get();
+            
+            $view->with('recentWorks', $recentWorks);
+        });
     }
 }
